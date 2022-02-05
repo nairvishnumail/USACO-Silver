3db203925eee4abe0fbfcadba5d0d196db42c3b0
nairvishnumail <76607707+nairvishnumail@users.noreply.github.com>
2022-02-04 23:12:35 -0800

    Update IcyPerimeter.java

--- IcyPerimeter.java
+++ IcyPerimeter.java
@@ -4,52 +4,7 @@
 //       Logic itself is correct
 //   http://www.usaco.org/index.php?page=viewproblem2&cpid=895
 
-import java.io.IOException;
 import java.util.Arrays;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
-
-class Kattio extends PrintWriter {
-	private BufferedReader r;
-	private StringTokenizer st;
-
-	// standard input
-	public Kattio() { this(System.in, System.out); }
-	public Kattio(InputStream i, OutputStream o) {
-		super(o);
-		r = new BufferedReader(new InputStreamReader(i));
-	}
-	// USACO-style file input
-	public Kattio(String problemName) throws IOException {
-		super(new FileWriter(problemName + ".out"));
-		r = new BufferedReader(new FileReader(problemName + ".in"));
-	}
-
-	// returns null if no more input
-	public String next() {
-		try {
-			while (st == null || !st.hasMoreTokens())
-				st = new StringTokenizer(r.readLine());
-			return st.nextToken();
-		} catch (Exception e) { }
-		return null;
-	}
-
-	public int nextInt() { return Integer.parseInt(next()); }
-	public double nextDouble() { return Double.parseDouble(next()); }
-	public long nextLong() { return Long.parseLong(next()); }
-}
-
-
-//.print()
-//.close()
 
 public class IcyPerimeter {
 	
@@ -65,7 +20,7 @@ public class IcyPerimeter {
 	
 	public static void main(String[] args) throws IOException {
 		
-		Kattio in = new Kattio("perimeter");
+		Kattio in = new Kattio(); // if using file input --> new Kattio("perimeter)  && add Kattio code to file
 		n = in.nextInt();
 		
 		arr = new char[n][n];
